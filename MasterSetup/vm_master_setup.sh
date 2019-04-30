57937ee368c9cd903d6dcd2478c8cdbe5768e01a
Matthew Yong <yongf@student.unimelb.edu.au>
2019-04-30 14:11:24 +1000

    Added adjustments to variables and changed node name to coucdb@<ip>

--- MasterSetup/vm_master_setup.sh
+++ MasterSetup/vm_master_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Environment Variables
+export user=SinsOnTwitter
+export pass=group68
+export masternode=172.26.38.38
+export node_slave_one=172.26.37.231
+export node_slave_two=172.26.38.62
+
 #Running docker container
 #Publishing on ports 5984, 9100-9200 , 5986 and 4369
 #Data storage at /mnt/couchdb/data
@@ -7,39 +14,17 @@ docker run -d -p 5984:5984 -p 9100-9200:9100-9200 -p 5986:5986 -p 4369:4369 -e C
 
 #Setting up configurations inside couchdb docker container
 docker exec couchdbone bash -c "echo \"-setcookie couchdbcluster\" >> /opt/couchdb/etc/vm.args"
-docker exec couchdbone bash -c "echo \"-name couchdbone@172.26.38.38\" >> /opt/couchdb/etc/vm.args"
+docker exec couchdbone bash -c "echo \"-name couchdb@172.26.38.38\" >> /opt/couchdb/etc/vm.args"
 docker exec couchdbone bash -c "echo \"-kernel inet_dist_listen_min 9100\" >> /opt/couchdb/etc/vm.args"
 docker exec couchdbone bash -c "echo \"-kernel inet_dist_listen_max 9200\" >> /opt/couchdb/etc/vm.args"
 
 #Restart to apply changes
 docker restart couchdbone
 
-#Variables
-export user=SinsOnTwitter
-export pass=group68
-export masternode=172.26.38.38
-export node_slave_one=172.26.37.231
-export node_slave_two=172.26.38.62
-
-#--Perform CURL Setup Commands for cluster setup--
-
 #Bind the clustered interface to all IP addresses availble on this machine
 curl -X PUT "http://${user}:${pass}@localhost:5984/_node/_local/_config/chttpd/bind_address" -d '"0.0.0.0"'
 
-#---Begin cluster setup---
-
-#Master Node
-curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
-    --header "Content-Type: application/json" \
-    --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \
-    \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": \"5984\", \
-    \"remote_node\": \"${masternode}\", \
-    \"remote_current_user\":\"${user}\", \"remote_current_password\":\"${pass}\"}"
-
-curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
-    --header "Content-Type: application/json" \
-    --data "{\"action\": \"add_node\", \"host\":\"${masternode}\", \
-    \"port\": \"5984\", \"username\": \"${user}\", \"password\":\"${pass}\"}"
+#---Begin cluster setup---#
 
 #Slave One
 curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
