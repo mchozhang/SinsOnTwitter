3943798c112737958af409f4c4be96cdd19315c7
Matthew Yong <yongf@student.unimelb.edu.au>
2019-04-30 14:38:38 +1000

    Changed all names to couchdb only

--- MasterSetup/vm_master_setup.sh
+++ MasterSetup/vm_master_setup.sh
@@ -10,16 +10,16 @@ export node_slave_two=172.26.38.62
 #Running docker container
 #Publishing on ports 5984, 9100-9200 , 5986 and 4369
 #Data storage at /mnt/couchdb/data
-docker run -d -p 5984:5984 -p 9100-9200:9100-9200 -p 5986:5986 -p 4369:4369 -e COUCHDB_USER=SinsOnTwitter -e COUCHDB_PASSWORD=group68 -v /mnt/couchdb/data:/opt/couchdb/data --name couchdbone couchdb:2.3.0
+docker run -d -p 5984:5984 -p 9100-9200:9100-9200 -p 5986:5986 -p 4369:4369 -e COUCHDB_USER=SinsOnTwitter -e COUCHDB_PASSWORD=group68 -v /mnt/couchdb/data:/opt/couchdb/data --name couchdb couchdb:2.3.0
 
 #Setting up configurations inside couchdb docker container
-docker exec couchdbone bash -c "echo \"-setcookie couchdbcluster\" >> /opt/couchdb/etc/vm.args"
-docker exec couchdbone bash -c "echo \"-name couchdb@172.26.38.38\" >> /opt/couchdb/etc/vm.args"
-docker exec couchdbone bash -c "echo \"-kernel inet_dist_listen_min 9100\" >> /opt/couchdb/etc/vm.args"
-docker exec couchdbone bash -c "echo \"-kernel inet_dist_listen_max 9200\" >> /opt/couchdb/etc/vm.args"
+docker exec couchdb bash -c "echo \"-setcookie couchdbcluster\" >> /opt/couchdb/etc/vm.args"
+docker exec couchdb bash -c "echo \"-name couchdb@172.26.38.38\" >> /opt/couchdb/etc/vm.args"
+docker exec couchdb bash -c "echo \"-kernel inet_dist_listen_min 9100\" >> /opt/couchdb/etc/vm.args"
+docker exec couchdb bash -c "echo \"-kernel inet_dist_listen_max 9200\" >> /opt/couchdb/etc/vm.args"
 
 #Restart to apply changes
-docker restart couchdbone
+docker restart couchdb
 
 #Bind the clustered interface to all IP addresses availble on this machine
 curl -X PUT "http://${user}:${pass}@localhost:5984/_node/_local/_config/chttpd/bind_address" -d '"0.0.0.0"'
@@ -31,7 +31,7 @@ curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
     --header "Content-Type: application/json" \
     --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \
     \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": \"5984\", \
-    \"remote_node\": \"${node_slave_one}\", \
+    \"node_count\": \"3\", \"remote_node\": \"${node_slave_one}\", \
     \"remote_current_user\":\"${user}\", \"remote_current_password\":\"${pass}\"}"
 
 curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
@@ -44,7 +44,7 @@ curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
     --header "Content-Type: application/json" \
     --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \
     \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": \"5984\", \
-    \"remote_node\": \"${node_slave_two}\", \
+    \"node_count\": \"3\", \"remote_node\": \"${node_slave_two}\", \
     \"remote_current_user\":\"${user}\", \"remote_current_password\":\"${pass}\"}"
 
 curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
@@ -55,6 +55,10 @@ curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
 #Command to finish the cluster setup
 curl -XPOST "http://${user}:${pass}@${masternode}:5984/_cluster_setup" \
     --header "Content-Type: application/json" --data "{\"action\": \"finish_cluster\"}" 
+
+#Removing nonode@nohost
+rev=`curl -XGET "http://${masternode}:5986/_nodes/nonode@nohost" --user "${user}:${pass}" | sed -e 's/[{}"]//g' | cut -f3 -d:`
+curl -X DELETE "http://${masternode}:5986/_nodes/nonode@nohost?rev=${rev}"  --user "${user}:${pass}"
 
 #Check to see membership details
 curl -XGET "http://${user}:${pass}@${masternode}:5984/_membership" | jq
--- SlaveTwoSetup/clean.sh
+++ SlaveTwoSetup/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Removing couchdb container on master node
-docker rm -f couchdbthree
+docker rm -f couchdb
 
 #Removing couchdb:2.3.0 image
 docker rmi couchdb:2.3.0
