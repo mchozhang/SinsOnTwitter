9fa70be4cc169bc344309c161714aa97d62cc6a4
Matthew Yong <yongf@student.unimelb.edu.au>
2019-04-30 14:54:36 +1000

    Added sleep between restarting containers

--- MasterSetup/vm_master_setup.sh
+++ MasterSetup/vm_master_setup.sh
@@ -12,6 +12,8 @@ export node_slave_two=172.26.38.62
 #Data storage at /mnt/couchdb/data
 docker run -d -p 5984:5984 -p 9100-9200:9100-9200 -p 5986:5986 -p 4369:4369 -e COUCHDB_USER=SinsOnTwitter -e COUCHDB_PASSWORD=group68 -v /mnt/couchdb/data:/opt/couchdb/data --name couchdb couchdb:2.3.0
 
+sleep 3
+
 #Setting up configurations inside couchdb docker container
 docker exec couchdb bash -c "echo \"-setcookie couchdbcluster\" >> /opt/couchdb/etc/vm.args"
 docker exec couchdb bash -c "echo \"-name couchdb@172.26.38.38\" >> /opt/couchdb/etc/vm.args"
@@ -20,6 +22,8 @@ docker exec couchdb bash -c "echo \"-kernel inet_dist_listen_max 9200\" >> /opt/
 
 #Restart to apply changes
 docker restart couchdb
+
+sleep 5
 
 #Bind the clustered interface to all IP addresses availble on this machine
 curl -X PUT "http://${user}:${pass}@localhost:5984/_node/_local/_config/chttpd/bind_address" -d '"0.0.0.0"'
