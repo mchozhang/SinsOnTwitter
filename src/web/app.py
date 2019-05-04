77cec2d0683c66af8d2425217089e8a443d962d8
mchozhnag <vivamcho@gmail.com>
2019-05-04 22:37:06 +1000

    add flask requirement

--- src/web/app.py
+++ src/web/app.py
@@ -7,21 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 # add more pages
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/map")
 def map():
     return render_template("map.html")
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
-# connect both pages with a navigation, parent templeate with child templates inherited from it
-
-
-# add CSS to website
-
-# Send it to the cloud
