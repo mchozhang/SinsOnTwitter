28c1c7ed528da03c5d061bbf8847de03e7eaf18c
Ivy Long <cacarianlong@gmail.com>
2019-05-06 13:32:47 +1000

    Add bar chart

--- src/web/app.py
+++ src/web/app.py
@@ -19,6 +19,27 @@ def map():
     return render_template("map.html")
 
 
+@app.route('/bar_chart')
+def bar():
+    return render_template("home.html", content="this is a testing")
+
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
 if __name__ == '__main__':
     app.run()
-
