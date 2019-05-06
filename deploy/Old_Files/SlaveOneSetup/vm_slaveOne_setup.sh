202747ed7e530038b7ab91827787e04e2de032c1
Matthew Yong <yongf@student.unimelb.edu.au>
2019-05-06 23:43:07 +1000

    Merged Ansible Script to master

--- deploy/Old_Files/SlaveOneSetup/vm_slaveOne_setup.sh
+++ deploy/Old_Files/SlaveOneSetup/vm_slaveOne_setup.sh
@@ -12,7 +12,7 @@ docker run -d -p 5984:5984 -p 9100-9200:9100-9200 -p 5986:5986 -p 4369:4369 -e C
 sleep 3
 
 docker exec couchdb bash -c "echo \"-setcookie couchdbcluster\" >> /opt/couchdb/etc/vm.args"
-docker exec couchdb bash -c "echo \"-name couchdb@172.26.38.62\" >> /opt/couchdb/etc/vm.args"
+docker exec couchdb bash -c "echo \"-name couchdb@172.26.37.231\" >> /opt/couchdb/etc/vm.args"
 docker exec couchdb bash -c "echo \"-kernel inet_dist_listen_min 9100\" >> /opt/couchdb/etc/vm.args"
 docker exec couchdb bash -c "echo \"-kernel inet_dist_listen_max 9200\" >> /opt/couchdb/etc/vm.args"
 docker restart couchdb
@@ -20,4 +20,9 @@ docker restart couchdb
 sleep 5
 
 #Bind the clustered interface to all IP addresses availble on this machine
-curl -X PUT "http://SinsOnTwitter:group68@localhost:5984/_node/_local/_config/chttpd/bind_address" -d '"0.0.0.0"'
+curl -X PUT "http://${user}:${pass}@localhost:5984/_node/_local/_config/chttpd/bind_address" -d '"0.0.0.0"'
+curl -X PUT "http://${user}:${pass}@localhost:5984/_node/_local/_config/httpd/bind_address" -d '"0.0.0.0"'
+
+
+
+
