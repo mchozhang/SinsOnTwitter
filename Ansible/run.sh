83840622bbd6de77066b67a1547d425a77cb40fb
Matthew Yong <yongf@student.unimelb.edu.au>
2019-05-07 18:11:07 +1000

    Able to Create Master and slaves

--- Ansible/run.sh
+++ Ansible/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Creating Instances
-#. ./pt-44401-openrc.sh; ansible-playbook --ask-become-pass nectar.yaml
-. ./unimelb-comp90024-group-68-openrc.sh; ansible-playbook nectar.yaml
+. ./pt-44401-openrc.sh; ansible-playbook nectar.yaml
+#. ./unimelb-comp90024-group-68-openrc.sh; ansible-playbook nectar.yaml
 echo Configuring and Setting Up Instances
 ansible-playbook -i inventory.ini -u ubuntu setup.yaml --private-key Group68KeyPair.pem
