411f7b1fe70af99c3502dd8ab2e6101330e61cd2
Matthew Yong <yongf@student.unimelb.edu.au>
2019-05-13 03:08:28 +1000

    Added Ansible Vault feature, fixed readme

--- deploy/Ansible/run.sh
+++ deploy/Ansible/run.sh
@@ -2,17 +2,11 @@
 
 echo Creating Instances
 . ./darren_pt-44443-openrc.sh; ansible-playbook nectar.yaml
-#. ./pt-44401-openrc.sh; ansible-playbook nectar.yaml
-#. ./unimelb-comp90024-group-68-openrc.sh; ansible-playbook nectar.yaml
 
 echo Configuring and Setting Up Instances
-ansible-playbook -i inventory.ini -u ubuntu setup.yaml
+ansible-playbook -i inventory.ini -u ubuntu setup.yaml --ask-vault-pass
 
-echo Setting Up the CouchDB Cluster
-ansible-playbook -i inventory.ini -u ubuntu workersetup.yaml
-
-echo Setting up Web Application
-ansible-playbook -i inventory.ini -u ubuntu websetup.yaml
+echo Setting Up Worker Nodes
+ansible-playbook -i inventory.ini -u ubuntu workersetup.yaml --ask-vault-pass
 
 sudo rm *.retry
-
